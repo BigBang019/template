549ec4b832a0fe81a43aaa027c9d1c03d116a3fc
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-08-02 20:50:57 +0800

    Update ST_2D.cpp

--- Data Structure/ST_2D.cpp
+++ Data Structure/ST_2D.cpp
@@ -1,46 +1,69 @@
 namespace ST_2D{
     using namespace std;
-    const int N = 3e2 + 5;
-    int n, m;
+    typedef long long ll;
+    const int N = 5e2 + 5;
     int maze[N][N];
-    int dp[N][N][9][9];
-    void ST(int n,int m){
-        for (int i = 1; i <= n;i++)
+    int LOG[N];
+    int dp[N][N][10][10];
+    int mp[N][N][10][10];
+    inline int Max(int a,int b){
+        return a > b ? a : b;
+    }
+    inline int Min(int a,int b){
+        return a < b ? a : b;
+    }
+    void init(){
+        for (register int i = 2; i <= N - 5; i++)
         {
-            for (int j = 1; j <= m;j++)
+            LOG[i] = LOG[i >> 1] + 1;
+        }
+    }
+    inline void ST(int n,int m){
+        for (register int i = 1; i <= n;i++)
+        {
+            for (register int j = 1; j <= m;j++)
             {
                 dp[i][j][0][0] = maze[i][j];
+                mp[i][j][0][0] = maze[i][j];
             }
         }
-        for (int k = 0; (1 << k) <= n;k++)
+        for (register int k = 0; (1 << k) <= n;k++)
         {
-            for (int l = 0; (1 << l) <= m;l++)
+            for (register int l = 0; (1 << l) <= m;l++)
             {
                 if (l==0 && k==0)
                     continue;
-                for (int i = 1; i + (1 << k) - 1 <= n;i++)
+                for (register int i = 1; i + (1 << k) - 1 <= n;i++)
                 {
-                    for (int j = 1; j + (1 << l) - 1 <= m;j++)
+                    for (register int j = 1; j + (1 << l) - 1 <= m;j++)
                     {
-                        if (k==0)
-                            dp[i][j][k][l] = max(dp[i][j][k][l - 1], dp[i][j + (1 << (l - 1))][k][l - 1]);
-                        else
-                            dp[i][j][k][l] = max(dp[i][j][k - 1][l], dp[i + (1 << (k - 1))][j][k - 1][l]);
+                        if (k==0){
+                            dp[i][j][k][l] = Max(dp[i][j][k][l - 1], dp[i][j + (1 << (l - 1))][k][l - 1]);
+                            mp[i][j][k][l] = Min(mp[i][j][k][l - 1], mp[i][j + (1 << (l - 1))][k][l - 1]);
+                        }
+                        else{
+                            dp[i][j][k][l] = Max(dp[i][j][k - 1][l], dp[i + (1 << (k - 1))][j][k - 1][l]);
+                            mp[i][j][k][l] = Min(mp[i][j][k - 1][l], mp[i + (1 << (k - 1))][j][k - 1][l]);
+                        }
                     }
                 }
             }
         }
     }
-    int query(int x1,int x2,int y1,int y2){
-        int k = 0, l = 0;
-        while((1<<(k+1))<=x2-x1+1)
-            k++;
-        while((1<<(l+1))<=y2-y1+1)
-            l++;
+    inline int query(int x1,int x2,int y1,int y2){
+        int k = LOG[x2 - x1 + 1], l = LOG[y2 - y1 + 1];
         int ans = dp[x1][y1][k][l];
-        ans = max(ans, dp[x1][y2 - (1 << l) + 1][k][l]);
-        ans = max(ans, dp[x2 - (1 << k) + 1][y1][k][l]);
-        ans = max(ans, dp[x2 - (1 << k) + 1][y2 - (1 << l) + 1][k][l]);
+        ans = Max(ans, dp[x1][y2 - (1 << l) + 1][k][l]);
+        ans = Max(ans, dp[x2 - (1 << k) + 1][y1][k][l]);
+        ans = Max(ans, dp[x2 - (1 << k) + 1][y2 - (1 << l) + 1][k][l]);
+        return ans;
+    }
+    inline int queryM(int x1,int x2,int y1,int y2){
+        int k = LOG[x2 - x1 + 1], l = LOG[y2 - y1 + 1];
+        int ans = mp[x1][y1][k][l];
+        ans = Min(ans, mp[x1][y2 - (1 << l) + 1][k][l]);
+        ans = Min(ans, mp[x2 - (1 << k) + 1][y1][k][l]);
+        ans = Min(ans, mp[x2 - (1 << k) + 1][y2 - (1 << l) + 1][k][l]);
         return ans;
     }
 }
