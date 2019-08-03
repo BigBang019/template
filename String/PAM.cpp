d006c463f94411e00b65594e0e1211b568a38b8a
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-08-03 23:59:29 +0800

    Update PAM.cpp

--- String/PAM.cpp
+++ String/PAM.cpp
@@ -1,36 +1,67 @@
+/***********************************************************************************
+    点：除奇/偶根节点外的每一个节点代表一种回文串
+    边：nxt[x][c]=y表示节点x表示的回文串左右加上字符c形成的字符串节点为y
+    ------------------------------------------------------------------------
+    fail[x]：x失配后跳转到的不等于自身的最长后缀回文子串
+        若fail[x]=y，y节点串一定是x节点串的后缀
+    len[x]：以x为结尾的最长回文子串的长度
+    cnt[x]：与以x结尾的最长回文子串相同的子串的个数
+    nxt[x][c]：编号为x的节点表示的回文串在两边添加字符c以后变成的回文串的编号
+    s[x]：第x次添加的字符（一开始设S[0] = -1，也可以是任意一个在串S中不会出现的字符）
+***********************************************************************************/
 struct PAM{
-    int nxt[N][26], fail[N], len[N];
-    int txt[N];
-    int tot, root0, root1, last, size;
-    void init(){
-        last = tot = size = 0;
-        txt[size] = -1;
-        root0 = newnode(0);
-        root1 = newnode(-1);
-        fail[root0] = 1;
-        fail[root1] = 0;
+    int nxt[N][27]; //next指针，next指针和字典树类似，指向的串为当前串两端加上同一个字符构成
+    int fail[N];    //fail指针，失配后跳转到fail指针指向的节点
+    int cnt[N];
+    int num[N];
+    int len[N]; //len[i]表示节点i表示的回文串的长度
+    int S[N];   //存放添加的字符
+    int last;   //指向上一个字符所在的节点，方便下一次add
+    int n;      //字符数组指针
+    int p;      //节点指针
+
+    int newnode(int l){ //新建节点
+        for (int i = 0; i < N; ++i)
+            nxt[p][i] = 0;
+        cnt[p] = 0;
+        num[p] = 0;
+        len[p] = l;
+        return p++;
     }
-    int newnode(int l){
-        len[tot] = l;
-        memset(nxt[tot], 0, sizeof(nxt[tot]));
-        tot++;
-        return tot - 1;
+
+    void init(){ //初始化
+        p = 0;
+        newnode(0);
+        newnode(-1);
+        last = 0;
+        n = 0;
+        S[n] = -1; //开头放一个字符集中没有的字符，减少特判
+        fail[0] = 1;
     }
-    int getfail(int x){
-        while(txt[size-len[x]-1]!=txt[size]) {
+
+    int get_fail(int x){ //和KMP一样，失配后找一个尽量最长的
+        while (S[n - len[x] - 1] != S[n])
             x = fail[x];
-        }
         return x;
     }
-    void extend(int c){
-        txt[++size] = c;
-        int now = getfail(last);
-        if(!nxt[now][c]){
-            int tmp = newnode(len[now] + 2);
-            fail[tmp] = nxt[getfail(fail[now])][c];
-            nxt[now][c] = tmp;
+
+    void add(int c){
+        S[++n] = c;
+        int cur = get_fail(last); //通过上一个回文串找这个回文串的匹配位置
+        if (!nxt[cur][c])
+        {                                            //如果这个回文串没有出现过，说明出现了一个新的本质不同的回文串
+            int now = newnode(len[cur] + 2);         //新建节点
+            fail[now] = nxt[get_fail(fail[cur])][c]; //和AC自动机一样建立fail指针，以便失配后跳转
+            nxt[cur][c] = now;
+            num[now] = num[fail[now]] + 1;
         }
-        last = nxt[now][c];
+        last = nxt[cur][c];
+        cnt[last]++;
     }
-    
+
+    void count(){
+        for (int i = p - 1; i >= 0; --i)
+            cnt[fail[i]] += cnt[i];
+        //父亲累加儿子的cnt，因为如果fail[v]=u，则u一定是v的子回文串！
+    }
 };
