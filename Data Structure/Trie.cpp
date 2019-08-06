4379e4a635bea4a52ab05f53cc950939101f0364
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-08-06 15:58:09 +0800

    Update Trie.cpp

--- Data Structure/Trie.cpp
+++ Data Structure/Trie.cpp
@@ -41,3 +41,38 @@ namespace TRIE{
     }
 }
 using namespace TRIE;
+
+/*****************************************************************************
+    多个Trie，update要改改
+*****************************************************************************/
+struct Trie{
+	struct NODE{
+		int nxt[B];
+		int cnt;
+		NODE() { memset(nxt, -1, sizeof(nxt)), cnt = 0; }
+		int &operator [] (ull i){
+			return nxt[i];
+		}
+	};
+	NODE &operator [] (ull i){
+		return po[i];
+	}
+	vector<NODE> po;
+	void init(){
+		po.clear();
+		po.pb(NODE());
+	}
+	void update(int a){
+		int now = 0;
+		for (int i = 29; i >= 0; i--)
+		{
+			bool x = a & (1 << i);
+			if (po[now][x]==-1){
+				po[now][x] = po.size();
+				po.pb(NODE());
+			}
+			now = po[now][x];
+			po[now].cnt++;
+		}
+	}
+};
